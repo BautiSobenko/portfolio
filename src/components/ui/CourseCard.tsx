3f82729f481fdab84fc7ee50fa9ab1e5ca21b169
Bautista Sobenko <bautipriano@gmail.com>
2024-04-15 11:52:27 -0300

    Grid on project cards

--- src/components/ui/CourseCard.tsx
+++ src/components/ui/CourseCard.tsx
@@ -12,26 +12,31 @@ export const CourseCard = () => {
           Projects through my career
         </p>
       </div>
-      <div className="grid h-[70rem] md:grid-cols-5 md:grid-rows-12 gap-4 text-center justify-center sm:grid-cols-2 md:max-w-[64rem] ">
-        <div className="p-6 rounded-2xl row-start-1 row-span-4 col-start-1 col-span-3 border border-black">
-          <div>
-
-          </div>
-          <div>
-            
-          </div>
-          {/* <Image 
-            src="#" 
-            alt="" 
-            width={500}
-            height={500}
-          /> */}
+      <div className="grid h-[80rem] md:grid-cols-5 md:grid-rows-12 gap-4 text-center justify-center sm:grid-cols-2 md:max-w-[64rem] ">
+        <div className="rounded-2xl row-start-1 row-span-4 col-start-1 col-span-3 border border-black grid md:grid-rows-[60%_40%]">
+          <div className="border border-black"></div>
+          <div className="border border-black"></div>
         </div>
-        <div className="p-6 rounded-2xl row-start-1 row-span-4 col-start-4 col-span-2 border border-black"></div>
-        <div className="p-6 rounded-2xl row-start-5 row-span-4 col-start-1 col-span-2 border border-black"></div>
-        <div className="p-6 rounded-2xl row-start-5 row-span-4 col-start-3 col-span-full border border-black"></div>
-        <div className="p-6 rounded-2xl row-start-9 row-span-4 col-start-1 col-span-3 border border-black"></div>
-        <div className="p-6 rounded-2xl row-start-9 row-span-4 col-start-4 col-span-full border border-black"></div>
+        <div className="rounded-2xl row-start-1 row-span-4 col-start-4 col-span-2 border border-black grid md:grid-rows-[60%_40%]">
+          <div className="border border-black"> </div>
+          <div className="border border-black"> </div>
+        </div>
+        <div className="rounded-2xl row-start-5 row-span-4 col-start-1 col-span-2 border border-black grid md:grid-rows-[60%_40%]">
+          <div className="border border-black"> </div>
+          <div className="border border-black"> </div>
+        </div>
+        <div className="rounded-2xl row-start-5 row-span-4 col-start-3 col-span-full border border-black grid md:grid-rows-[40%_60%]">
+          <div className="border border-black"> </div>
+          <div className="border border-black"> </div>
+        </div>
+        <div className="rounded-2xl row-start-9 row-span-4 col-start-1 col-span-3 border border-black grid md:grid-rows-[60%_40%]">
+          <div className="border border-black"> </div>
+          <div className="border border-black"> </div>
+        </div>
+        <div className="rounded-2xl row-start-9 row-span-4 col-start-4 col-span-full border border-black grid md:grid-rows-[60%_40%]">
+          <div className="border border-black"> </div>
+          <div className="border border-black"> </div>
+        </div>
       {/* {projectsData.map((project, index) => (
         <Card
           key={project.name}
