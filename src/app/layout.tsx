70b8446108b8a40f8b4d7b658e23faca88e1817f
Bautista Sobenko <bautipriano@gmail.com>
2024-06-09 20:35:00 -0300

    Contact section working. Metadata update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,17 +7,19 @@ import { aeonik, sohne } from './utils/fonts'
 
 export const metadata: Metadata = {
   title: "Bautista Sobenko | Portfolio",
-  description:
-    "Bautista Sobenko is a last-year Software Engineer Student at National Univerisity of Mar del Plata",
-};
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+  description: "Bautista Sobenko is a last-year Software Engineer Student at National Univerisity of Mar del Plata",
+  applicationName: "Bautista Priano Sobenko Portfolio",
+  authors: [{name: "Bautista Priano Sobenko"}],
+  keywords: ["Bautista", "Priano", "Sobenko", "Portfolio", "Software Engineer", "Student", "National University of Mar del Plata"],
+  icons: "/web-icon.svg",
+};  
+
+export default function RootLayout({children} : {children: React.ReactNode }) {
   return (
     <html lang="en" className={`${aeonik} ${sohne}`}>
-      <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+      <head>
+        <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+      </head>
       <body className={`antialiased bg-bg-black text-text-100`}>
         <Announcement />
         <Header />
--- src/components/ui/contact/ContactSection.tsx
+++ src/components/ui/contact/ContactSection.tsx
@@ -6,7 +6,7 @@ export const ContactSection = () => {
   const { handleSubmit } = useFormSubmit();
 
   return (
-    <section className="mb-24">
+    <section className="mb-24 lg:px-24 xl:px-52 mx-10">
       <div className="text-center py-10">
         <h2 className="text-5xl font-bold mb-3">Contact me</h2>
         <p className="text-sm md:text-base">
@@ -14,22 +14,22 @@ export const ContactSection = () => {
         </p>
       </div>
 
-      <form method="POST" className="px-40 space-y-4 my-4">
-        <div className="flex gap-2">
-          <div className="flex-[0_1_50%]">
-            <label htmlFor="name" className="block mb-2 text-base font-medium text-text-100">Your name</label>
-            <input type="text" name="name" required className="block p-2.5 w-full text-base text-text-300 focus:border-primary-100 focus:ring-primary-100 bg-bg-100 rounded-lg border outline-none border-bg-200 placeholder-text-300/50" placeholder=":)"/>
+      <form onSubmit={handleSubmit} className="my-4 space-y-4 text-xs md:text-sm lg:text-base">
+        <div className="flex flex-col md:flex-row gap-4 md:gap-2">
+          <div className="md:flex-[0_1_50%]">
+            <label htmlFor="name" className="block mb-2 font-medium text-text-100">Name</label>
+            <input type="text" name="name" required className="block p-2.5 w-full text-text-300 focus:border-primary-100 focus:ring-primary-100 bg-bg-100 rounded-lg border outline-none border-bg-200 placeholder-text-300/50" placeholder="Your name"/>
           </div>
-          <div className="flex-[0_0_50%]">
-            <label htmlFor="email" className="block mb-2 text-base font-medium text-text-100">Your email</label>
-            <input type="email" name="email" required className="block p-2.5 w-full text-base text-text-300 focus:border-primary-100 focus:ring-primary-100 bg-bg-100 rounded-lg border outline-none border-bg-200 placeholder-text-300/50" placeholder="email@gmail.com"/>
+          <div className="md:flex-[0_0_50%]">
+            <label htmlFor="email" className="block mb-2 font-medium text-text-100">Email</label>
+            <input type="email" name="email" required className="block p-2.5 w-full text-text-300 focus:border-primary-100 focus:ring-primary-100 bg-bg-100 rounded-lg border outline-none border-bg-200 placeholder-text-300/50" placeholder="Your email"/>
           </div>
         </div>
         <div>
-          <label htmlFor="message" className="block mb-2 text-base font-medium text-text-100">Your message</label>
-          <textarea id="message" name="message" required rows={4} className="p-2.5 min-h-32 w-full text-base text-text-300 focus:border-primary-100 focus:ring-primary-100 bg-bg-100 rounded-lg border outline-none border-bg-200 placeholder-text-300/50" placeholder="Leave a comment..."></textarea>
+          <label htmlFor="message" className="block mb-2 font-medium text-text-100">Message</label>
+          <textarea id="message" name="message" required rows={4} className="p-2.5 min-h-32 w-full text-text-300 focus:border-primary-100 focus:ring-primary-100 bg-bg-100 rounded-lg border outline-none border-bg-200 placeholder-text-300/50" placeholder="Your message"></textarea>
         </div>
-        <input type="submit" value="Send Message" className="py-3.5 text-center inline-block bg-accent-100 w-full rounded-lg font-medium" />
+        <input type="submit" value="Send Message" className="py-3 text-center inline-block bg-accent-100 w-full rounded-lg font-medium hover:bg-bg-100 border-2 border-accent-100 hover:border-accent-100 " />
       </form>
 
     </section>
