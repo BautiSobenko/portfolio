35882ec144c11f86cdb9150f06bd5359f0a7fba9
Bautista Sobenko <bautipriano@gmail.com>
2024-06-17 22:18:10 -0300

    .

--- src/lib/projectsData.ts
+++ src/lib/projectsData.ts
@@ -3,7 +3,7 @@ export const projectsData = [
     name: 'Real-time military aircraft recognition (YoloV7 object detection model)',
     alt: 'Photo of a military aircraft representing an input of an Artificial Intelligence recognition model',
     description: 'Final team project for the subject "Artificial Intelligence" adressing Computer Vision, Object Classification and Detection and Convolutional Neural Networks.',
-    src: '/ai.svg',
+    src: '/portfolio/ai.svg',
     topic: 'Artificial Intelligence + Computer Vision',
     href: 'https://github.com/BautiSobenko/IA-2022',
     tags: [
@@ -36,7 +36,7 @@ export const projectsData = [
     name: 'Database Design and implementation for security control in a laboratory.',
     alt: '',
     description: 'Final team project for the subject "Databases" where a solution to a real-world problem is implemented using PostgreSQL as the engine.',
-    src: '/database.svg',
+    src: '/portfolio/database.svg',
     topic: 'Database Design and Management',
     href: 'https://github.com/BautiSobenko/app-bd',
     tags: [
