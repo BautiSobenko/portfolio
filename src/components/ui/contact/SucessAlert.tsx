e4fdd1fc1e1cafd19fca08aed6050165832425fc
Bautista Sobenko <bautipriano@gmail.com>
2024-06-20 20:13:24 -0300

    .

--- src/components/ui/contact/SucessAlert.tsx
+++ src/components/ui/contact/SucessAlert.tsx
@@ -2,7 +2,7 @@ export default function SucessAlert() {
   
   return (
     <div className="top-0 left-0 fixed rounded-md bg-[#4ade80] w-full p-3">
-      <div className="flex items-center justify-center gap-1">
+      <div className="flex items-center justify-center gap-2">
           <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="size-6 text-text-100">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
